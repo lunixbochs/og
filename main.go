5354dc9e7627c854fc217686957989a4585a7ecc
Ryan Hileman <lunixbochs@gmail.com>
2014-10-12 14:41:27 -0700

    preliminary plugin conversion

--- main.go
+++ main.go
@@ -1,52 +1,29 @@
 package main
 
 import (
+	"./plugins"
+	"fmt"
 	"go/parser"
 	"go/token"
 	"log"
+	"os"
 )
 
 func main() {
-	src := `
-package main
-
-import (
-	"fmt"
-	"log"
-)
-
-func next(int n) (int, error) {
-	return n, nil
-}
-
-func call(n int) (int, error) {
-	fmt.Println(n)
-	try(next(-1))
-	output := try(next(-1), FATAL)
-	output = try(next(-1), FATAL, "message")
-	return output, nil
-}
-
-func main() {
-	fmt.Println("hi")
-	output := try(call(1), FATAL, "message")
-	output = try(call(2), "message")
-	try(call(3), func(err error) {fmt.Println(err)})
-	try(call(4), RETURN)
-	try(call(5))
-	try(call(6), func() {return 1})
-	fmt.Println("end", output)
-}
-`
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./og <source filename.go>")
+		fmt.Println("  Example: ./og examples/try/basic.go")
+		return
+	}
 	fset := token.NewFileSet()
-	f, err := parser.ParseFile(fset, "src.go", src, 0)
+	f, err := parser.ParseFile(fset, os.Args[1], nil, 0)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Print("input:")
-	PrintCode(fset, f)
-	ExpandTry(fset, f)
+	plugins.PrintCode(fset, f)
+	plugins.ExpandTry(fset, f)
 	log.Print("output:")
-	PrintCode(fset, f)
+	plugins.PrintCode(fset, f)
 }
